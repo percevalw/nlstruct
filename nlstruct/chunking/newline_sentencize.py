87f4ca9dab2cb52890377663207192313f311bd5
Perceval Wajsburt <perceval.wajsburt@gmail.com>
2020-03-04 14:16:38 +0100

    added minimum sentence length for newline_sentencize

--- nlstruct/chunking/newline_sentencize.py
+++ nlstruct/chunking/newline_sentencize.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 from nlstruct.core.cache import cached
 
 
-def newline_sentencize_(docs, max_sentence_length=None, n_threads=1,
+def newline_sentencize_(docs, max_sentence_length=None, min_sentence_length=None, n_threads=1,
                         reg_split=r"((?:\s*\n)+\s*)",
                         reg_token=r"[\w*]+|[^\w\s\n*]",
                         with_tqdm=True):
@@ -46,32 +46,38 @@ def newline_sentencize_(docs, max_sentence_length=None, n_threads=1,
     sentences = []
     for doc_id, txt in zip(docs["doc_id"], (tqdm(docs["text"], desc="Splitting docs into sentences") if with_tqdm else docs["text"])):
         idx = 0
+        queued_spans = []
         sentence_id = 0
         for i, part in enumerate(reg_split.split(txt)):
             if i % 2 == 0:  # we're in a sentence
-                spans = [(m.start() + idx, m.end() + idx) for m in reg_token.finditer(part)]
+                queued_spans.extend([(m.start() + idx, m.end() + idx) for m in reg_token.finditer(part)])
+                if min_sentence_length is not None and len(queued_spans) < min_sentence_length:
+                    idx += len(part)
+                    continue
                 last = 0
                 if max_sentence_length is None:
-                    max_sentence_length_ = len(spans)
+                    max_sentence_length_ = len(queued_spans)
                 else:
                     max_sentence_length_ = max_sentence_length
-                for j in range(last + max_sentence_length_, len(spans) - max_sentence_length_, max(max_sentence_length_, 1)):
-                    b = spans[last][0]
-                    e = spans[j - 1][1]
+                for j in range(last + max_sentence_length_, len(queued_spans) - max_sentence_length_, max(max_sentence_length_, 1)):
+                    b = queued_spans[last][0]
+                    e = queued_spans[j - 1][1]
                     doc_ids.append(doc_id)
                     sentence_ids.append(sentence_id)
                     begins.append(b)
                     ends.append(e)
+                    queued_spans = []
                     sentences.append(txt[b:e])
                     last = j
                     sentence_id += 1
-                if last < len(spans):
-                    b = spans[last][0]
-                    e = spans[-1][1]
+                if last < len(queued_spans):
+                    b = queued_spans[last][0]
+                    e = queued_spans[-1][1]
                     doc_ids.append(doc_id)
                     sentence_ids.append(sentence_id)
                     begins.append(b)
                     ends.append(e)
+                    queued_spans = []
                     sentences.append(txt[b:e])
                     sentence_id += 1
             idx += len(part)
@@ -85,10 +91,10 @@ def newline_sentencize_(docs, max_sentence_length=None, n_threads=1,
 
 
 @cached.will_ignore(("n_threads", "with_tqdm"))
-def mimic_sentencize(texts, max_sentence_length=None, n_threads=1, with_tqdm=True):
-    return newline_sentencize_(texts, max_sentence_length, reg_split=r"(\s*\n\s*\n\s*)", n_threads=n_threads, with_tqdm=with_tqdm)
+def mimic_sentencize(texts, max_sentence_length=None, min_sentence_length=None, n_threads=1, with_tqdm=True):
+    return newline_sentencize_(texts, max_sentence_length, min_sentence_length, reg_split=r"(\s*\n\s*\n\s*)", n_threads=n_threads, with_tqdm=with_tqdm)
 
 
 @cached.will_ignore(("n_threads", "with_tqdm"))
-def newline_sentencize(texts, max_sentence_length=None, n_threads=1, with_tqdm=True):
-    return newline_sentencize_(texts, max_sentence_length, reg_split=r"((?:\s*\n){1,}\s*)", n_threads=n_threads, with_tqdm=with_tqdm)
+def newline_sentencize(texts, max_sentence_length=None, min_sentence_length=None, n_threads=1, with_tqdm=True):
+    return newline_sentencize_(texts, max_sentence_length, min_sentence_length, reg_split=r"((?:\s*\n){1,}\s*)", n_threads=n_threads, with_tqdm=with_tqdm)
