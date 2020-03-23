cb3632a6e1966ea2d55fe02430af6b43a1dfcdef
Perceval Wajsburt <perceval.wajsburt@gmail.com>
2020-03-23 16:54:28 +0100

    more general wordpiece preprocessing to extract char maps

--- nlstruct/chunking/huggingface_tokenizer.py
+++ nlstruct/chunking/huggingface_tokenizer.py
@@ -7,19 +7,25 @@ def huggingface_tokenize(docs, tokenizer):
     begins = []
     ends = []
     token_idx = []
-
+    special_tokens = [t for token in tokenizer.special_tokens_map.values() for t in ((token,) if isinstance(token, str) else token)]
+    if hasattr(tokenizer, 'sp_model'):
+        special_tokens += ["▁"]
+    else:
+        special_tokens += ["</w>"]
     for doc_id, text in zip(docs["doc_id"], docs["text"]):
-        token_id = 0
         i = 0
+        token_id = 0
 
-        # Sentence
         lookuptext = text.lower()
         for piece in tokenizer.convert_ids_to_tokens(tokenizer.encode(text)):
             doc_ids.append(doc_id)
             tokens.append(piece)
-            piece_size = len(piece) - (4 if (piece.endswith("</s>") or piece.endswith("</w>")) else 0)
-            delta = lookuptext[i:].find(piece[:min(piece_size, 1)].lower())
-            assert delta < 50, (lookuptext[i:i + 50], piece[:min(piece_size, 1)].lower())
+            striped_piece = piece
+            for special in special_tokens:
+                striped_piece = striped_piece.replace(special, "")
+            piece_size = len(striped_piece)
+            delta = lookuptext[i:].find(striped_piece.lower())
+            assert 0 <= delta < 50, (lookuptext[i:i + 50], striped_piece.lower())
             i += delta
             begins.append(i)
             i += piece_size
@@ -27,9 +33,12 @@ def huggingface_tokenize(docs, tokenizer):
             token_idx.append(token_id)
             token_id += 1
     tokens = pd.DataFrame({"doc_id": doc_ids, "token_id": range(len(token_idx)), "token_idx": token_idx, "token": tokens, "begin": begins, "end": ends})
-    voc = [None] * len(tokenizer.encoder)
-    for token, token_i in tokenizer.encoder.items():
-        voc[token_i] = token
+    voc = tokenizer.convert_ids_to_tokens(list(range(tokenizer.vocab_size)))
+    counts = {}
+    for i, token in enumerate(list(voc)):
+        counts[token] = counts.get(token, 0) + 1
+        if counts[token] > 1:
+            voc[i] = token + "-{}".format(i)
     token_voc = pd.CategoricalDtype(voc)
     tokens = tokens.astype({"doc_id": docs["doc_id"].dtype, "token": token_voc})
     return tokens
