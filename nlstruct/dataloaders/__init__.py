ac681cfecc415c0b01ec01bdc978c45f3ba6f296
Perceval Wajsburt <perceval.wajsburt@gmail.com>
2020-03-27 13:31:41 +0100

    dataloaders exports from __init__

---
+++ nlstruct/dataloaders/__init__.py
@@ -0,0 +1,6 @@
+from nlstruct.dataloaders.brat import load_from_brat
+from nlstruct.dataloaders.ncbi_disease import load_ncbi_disease
+from nlstruct.dataloaders.medic import load_medic_synonyms, load_alt_medic_mapping
+from nlstruct.dataloaders.bc5cdr import load_bc5cdr
+from nlstruct.dataloaders.quaero import load_quaero
+from nlstruct.dataloaders.n2c2_2019_task3 import load_n2c2_2019_task3
