0cf9d6a8ff2b21085d83bdea9cb122e771f94ae9
Perceval Wajsburt <perceval.wajsburt@gmail.com>
2020-04-03 13:51:36 +0200

    allow passthrough arguments for huggingface_tokenizer

--- nlstruct/chunking/huggingface_tokenizer.py
+++ nlstruct/chunking/huggingface_tokenizer.py
@@ -3,7 +3,7 @@ import unidecode
 from tqdm import tqdm
 
 
-def huggingface_tokenize(docs, tokenizer, with_tqdm=False):
+def huggingface_tokenize(docs, tokenizer, with_tqdm=False, **kwargs):
     doc_ids = []
     tokens = []
     begins = []
@@ -11,12 +11,12 @@ def huggingface_tokenize(docs, tokenizer, with_tqdm=False):
     token_idx = []
     special_tokens = [t for token in tokenizer.special_tokens_map.values() for t in ((token,) if isinstance(token, str) else token)]
     special_tokens += ["▁", "##", "</w>"]
-    for doc_id, text in tqdm(zip(docs["doc_id"], docs["text"]), disable=not with_tqdm, total=len(docs)):
+    for doc_id, text in tqdm(zip(docs["doc_id"], docs["text"]), disable=not with_tqdm, total=len(docs), leave=False, desc="Tokenizing"):
         i = 0
         token_id = 0
 
         lookuptext = unidecode.unidecode(text.lower())
-        for piece in tokenizer.convert_ids_to_tokens(tokenizer.encode(text)):
+        for piece in tokenizer.convert_ids_to_tokens(tokenizer.encode(text, **kwargs)):
             doc_ids.append(doc_id)
             tokens.append(piece)
             striped_piece = piece
