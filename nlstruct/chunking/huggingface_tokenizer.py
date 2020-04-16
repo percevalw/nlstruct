48e2bc509584a569c6d39a1af2a04e827ea4889e
Perceval Wajsburt <perceval.wajsburt@gmail.com>
2020-04-16 11:52:10 +0200

    option to change the default document col name in chunking functions

--- nlstruct/chunking/huggingface_tokenizer.py
+++ nlstruct/chunking/huggingface_tokenizer.py
@@ -5,7 +5,7 @@ import pandas as pd
 from tqdm import tqdm
 
 
-def huggingface_tokenize(docs, tokenizer, with_tqdm=False, with_token_spans=True, **kwargs):
+def huggingface_tokenize(docs, tokenizer, with_tqdm=False, with_token_spans=True, doc_id_col="doc_id", text_col="text", **kwargs):
     doc_ids = []
     tokens = []
     begins = []
@@ -15,7 +15,7 @@ def huggingface_tokenize(docs, tokenizer, with_tqdm=False, with_token_spans=True
     if with_token_spans:
         special_tokens = [t for token in tokenizer.special_tokens_map.values() for t in ((token,) if isinstance(token, str) else token)]
         special_tokens += ["▁", "##", "</w>"]
-        for doc_id, text in tqdm(zip(docs["doc_id"], docs["text"]), disable=not with_tqdm, total=len(docs), leave=False, desc="Tokenizing"):
+        for doc_id, text in tqdm(zip(docs[doc_id_col], docs[text_col]), disable=not with_tqdm, total=len(docs), leave=False, desc="Tokenizing"):
             i = 0
             token_id = 0
 
@@ -42,16 +42,16 @@ def huggingface_tokenize(docs, tokenizer, with_tqdm=False, with_token_spans=True
                 ends.append(i)
                 token_idx.append(token_id)
                 token_id += 1
-        tokens = pd.DataFrame({"doc_id": doc_ids, "token_id": range(len(token_idx)), "token_idx": token_idx, "token": tokens, "begin": begins, "end": ends})
+        tokens = pd.DataFrame({doc_id_col: doc_ids, "token_id": range(len(token_idx)), "token_idx": token_idx, "token": tokens, "begin": begins, "end": ends})
     else:
-        for doc_id, text in tqdm(zip(docs["doc_id"], docs["text"]), disable=not with_tqdm, total=len(docs), leave=False, desc="Tokenizing"):
+        for doc_id, text in tqdm(zip(docs[doc_id_col], docs[text_col]), disable=not with_tqdm, total=len(docs), leave=False, desc="Tokenizing"):
             token_id = 0
             for encoded_piece in tokenizer.convert_ids_to_tokens(tokenizer.encode(text, **kwargs)):
                 doc_ids.append(doc_id)
                 tokens.append(encoded_piece)
                 token_idx.append(token_id)
                 token_id += 1
-        tokens = pd.DataFrame({"doc_id": doc_ids, "token_id": range(len(token_idx)), "token_idx": token_idx, "token": tokens})
+        tokens = pd.DataFrame({doc_id_col: doc_ids, "token_id": range(len(token_idx)), "token_idx": token_idx, "token": tokens})
 
     voc = tokenizer.convert_ids_to_tokens(list(range(tokenizer.vocab_size)))
     counts = {}
@@ -60,5 +60,6 @@ def huggingface_tokenize(docs, tokenizer, with_tqdm=False, with_token_spans=True
         if counts[token] > 1:
             voc[i] = token + "-{}".format(i)
     token_voc = pd.CategoricalDtype(voc)
-    tokens = tokens.astype({"doc_id": docs["doc_id"].dtype, "token": token_voc})
+    tokens = tokens.astype({doc_id_col: docs[doc_id_col].dtype, "token": token_voc})
+    tokens = tokens.merge(docs[[doc_id_col] + [col for col in docs.columns if col not in tokens.columns and col != "text"]])
     return tokens
