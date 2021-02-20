c4f3befccda6fba44fea7bca46d7a81aa00d5430
Perceval Wajsburt <perceval.wajsburt@gmail.com>
2021-02-20 19:39:37 +0100

    added export in dataloaders init

--- pyner/dataloaders/__init__.py
+++ pyner/dataloaders/__init__.py
@@ -3,3 +3,8 @@ from pyner.dataloaders.deft import DEFT
 from pyner.dataloaders.genia import GENIA
 from pyner.dataloaders.ncbi import NCBI
 from pyner.dataloaders.quaero import QUAERO
+from pyner.dataloaders.bc5cdr import BC5CDR
+from pyner.dataloaders.i2b2_temporal import I2B2Temporal
+
+from pyner.dataloaders.umls import UMLS
+from pyner.dataloaders.medic import MEDIC
